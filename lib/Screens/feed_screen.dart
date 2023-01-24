5ded01caf4a52d0d398b239183e4c33618012fec
Kunal Dua <kunaldua2000@gmail.com>
2023-01-24 11:27:21 +0530

    cooment updated

--- lib/Screens/feed_screen.dart
+++ lib/Screens/feed_screen.dart
@@ -20,7 +20,10 @@ class FeedScreen extends StatelessWidget {
         ],
       ),
       body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection("posts").snapshots(),
+          stream: FirebaseFirestore.instance
+              .collection("posts")
+              .orderBy("datePublished", descending: true)
+              .snapshots(),
           builder: (context,
               AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
